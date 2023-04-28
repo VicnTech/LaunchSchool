f93bc1e6454bcf7c8182b5ac83433ce56a575bd7
VicnTech <49599043+VicnTech@users.noreply.github.com>
2023-04-28 16:35:33 -0500

    Submitting Calculator Bonus Features work

--- JS101/calculator.js
+++ JS101/calculator.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const findMessage = require('./calculator_messages.json');
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -8,61 +9,66 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
+
 prompt('Welcome to Calcultaor!');
+let anotherCalc = true;
+while (anotherCalc) {
+  // Ask the user for the first number.
+  prompt("What's the first number?");
+  let number1 = readline.question();
 
-// Ask the user for the first number.
-prompt("What's the first number?");        
-let number1 = readline.question();
+  while (invalidNumber(number1)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number1 = readline.question();
+  }
 
-while (invalidNumber(number1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
-}
+  // Ask the user for the second number.
+  prompt("What's the second number?");
+  let number2 = readline.question();
 
-// Ask the user for the second number.
-prompt("What's the second number?");       
-let number2 = readline.question();
+  while (invalidNumber(number2)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number2 = readline.question();
+  }
 
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
-}
+  // Ask the user for an operation to perform.
+  prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+  let operation = readline.question();
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Must choose 1, 2, 3, or 4');
+    operation = readline.question();
+  }
 
-// Ask the user for an operation to perform.
-console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide'); 
-let operation = readline.question();
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3, or 4');
-  operation = readline.question();
-}
+  // Perform the operation on the two numbers.
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
 
-// Perform the operation on the two numbers.
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
+  // Print the result to the terminal.
+  prompt(`The result is: ${output}`);
 
-// Alternative code using if/else
-// if (operation === '1') {                        // '1' represents addition
-//   output = Number(number1) + Number(number2);
-// } else if (operation === '2') {                 // '2' represents subtraction
-//   output = Number(number1) - Number(number2);
-// } else if (operation === '3') {                 // '3' represents multiply
-//   output = Number(number1) * Number(number2);
-// } else if (operation === '4') {                 // '4' represents divide
-//   output = Number(number1) / Number(number2);
-// }
-
-// Print the result to the terminal.
-console.log(`The result is: ${output}`);
+  // New calcuation? prompt
+  let continueLoop = true;
+  while (continueLoop) {
+    prompt("Would you like to perform another calculation? Y/N?");
+    let yesOrNo = readline.question();
+    if (yesOrNo.toLowerCase()[0] === "no") {
+      anotherCalc = false;
+      continueLoop = false;
+    } else if (yesOrNo.toLowerCase()[0] === "yes") {
+      continueLoop = false;
+    } else {prompt('Your answer is not a "Yes" or "No".')}
+  }
+} //end while anotherCalc loop
