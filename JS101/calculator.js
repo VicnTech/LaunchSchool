269c221f326d9b7242b1e2036d18152032de4d0f
vicntech <49599043+VicnTech@users.noreply.github.com>
2023-04-28 22:12:18 -0500

    Completed Calculator with Bonus Features

--- JS101/calculator.js
+++ JS101/calculator.js
@@ -1,41 +1,64 @@
 const readline = require('readline-sync');
-const findMessage = require('./calculator_messages.json');
+const MESSAGES = require('./calculator_messages.json');
 
 function prompt(message) {
   console.log(`=> ${message}`);
+}
+
+function message(lang, message) {
+  return MESSAGES[lang][message];
 }
 
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
+prompt(`1) English? 2) Español? 3) Français?`);
+let language = readline.question();
+let continueLoop = true;
+while (continueLoop) {
+  switch (language) {
+    case '1':
+      language = "english";
+      continueLoop = false;
+      break;
+    case '2':
+      language = "spanish";
+      continueLoop = false;
+      break;
+    case '3':
+      language = "french";
+      continueLoop = false;
+      break;
+  }
+}
 
-prompt('Welcome to Calcultaor!');
+prompt(message(language, 'welcome'));
 let anotherCalc = true;
 while (anotherCalc) {
   // Ask the user for the first number.
-  prompt("What's the first number?");
+  prompt(message(language, 'askFirstNum'));
   let number1 = readline.question();
 
   while (invalidNumber(number1)) {
-    prompt("Hmm... that doesn't look like a valid number.");
+    prompt(message(language, 'sayInvalid'));
     number1 = readline.question();
   }
 
   // Ask the user for the second number.
-  prompt("What's the second number?");
+  prompt(message(language, 'askSecondNum'));
   let number2 = readline.question();
 
   while (invalidNumber(number2)) {
-    prompt("Hmm... that doesn't look like a valid number.");
+    prompt(message(language, 'sayInvalid'));
     number2 = readline.question();
   }
 
   // Ask the user for an operation to perform.
-  prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+  prompt(message(language, 'askOperation'));
   let operation = readline.question();
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt('Must choose 1, 2, 3, or 4');
+    prompt(message(language, 'sayNotOperator'));
     operation = readline.question();
   }
 
@@ -57,18 +80,18 @@ while (anotherCalc) {
   }
 
   // Print the result to the terminal.
-  prompt(`The result is: ${output}`);
+  prompt(`${message(language, 'sayResult')}${output}`);
 
   // New calcuation? prompt
-  let continueLoop = true;
+  continueLoop = true;
   while (continueLoop) {
-    prompt("Would you like to perform another calculation? Y/N?");
+    prompt(message(language, 'askAnother'));
     let yesOrNo = readline.question();
-    if (yesOrNo.toLowerCase()[0] === "no") {
+    if (yesOrNo.toLowerCase()[0] === "n") {
       anotherCalc = false;
       continueLoop = false;
-    } else if (yesOrNo.toLowerCase()[0] === "yes") {
+    } else if (yesOrNo.toLowerCase()[0] === "y") {
       continueLoop = false;
-    } else {prompt('Your answer is not a "Yes" or "No".')}
+    } else {prompt(message(language, 'sayNotYN'))}
   }
 } //end while anotherCalc loop
